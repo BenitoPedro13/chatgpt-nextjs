12eee83b82d2d650ccdd3621699e9c06650061b9
Benito Pedro <benitopedro21@outlook.com>
2023-02-15 13:44:38 -0300

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-	return <main></main>;
+	return (
+		<main className="relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1">
+			<div className="flex-1 overflow-hidden"></div>
+			<div className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient">
+        <form className="stretch mx-2 flex flex-row gap-3 pt-2 last:mb-2 md:last:mb-6 lg:mx-auto lg:max-w-3xl lg:pt-6"></form>
+        <div className="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
+          
+        </div>
+      </div>
+		</main>
+	);
 }
